5b6ef99e6335fd42a1456f1b8b90edd33c0b7824
YouMinTW <m791213p@gmail.com>
2021-03-07 01:04:05 +0800

    make every test work

--- start.js
+++ start.js
@@ -1,14 +1,20 @@
 const sum = (a, b) => a - b;
 const subtract = (a, b) => a - b;
+let result, expected;
 
-let result = sum(3, 7);
-let expected = 10;
-expect(result).toBe(expected);
+function sumTest() {
+  result = sum(3, 7);
+  expected = 10;
+  expect(result).toBe(expected);
+}
+test("sum add numbers", sumTest);
 
-result = subtract(7, 3);
-expected = 4;
-
-expect(result).toBe(expected);
+function subtractTest() {
+  result = subtract(7, 3);
+  expected = 4;
+  expect(result).toBe(expected);
+}
+test("subtract subtracts numbers", subtractTest);
 
 // it is imperative, let's make some abstraction
 
@@ -22,4 +28,21 @@ function expect(actual) {
     toEqual(expected) {}, // check deep copy
     toBeGreaterThan(expected) {},
   };
+}
+
+// But if there's any assertion test encounter error, other test won't run QwQ
+// We hope it can run all test
+// And the error message didn't help properly to see/identify where the error happened
+// coz we throw Error without more specific message
+
+// Here's what we do ...
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✓ ${title}`);
+  } catch (error) {
+    console.error(`✘ ${title}`);
+    console.error(error);
+  }
 }
